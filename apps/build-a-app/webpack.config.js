072469e57490b771064faf0296e5dd375db0f148
htwnavarro <herontnavarro@gmail.com>
2024-10-07 16:34:50 -0700

    nx enhanced webpack config for sass loading

--- apps/build-a-app/webpack.config.js
+++ apps/build-a-app/webpack.config.js
@@ -1,36 +1,37 @@
-const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
-const { NxReactWebpackPlugin } = require('@nx/react/webpack-plugin');
-const { join } = require('path');
+const { composePlugins, withNx, withWeb } = require('@nx/webpack');
+const { withReact } = require('@nx/react');
+const { runWebpackDevServer } = require('@nx/webpack/src/utils/run-webpack');
+const path = require('node:path');
 
-module.exports = {
-    output: {
-        path: join(__dirname, '../../dist/apps/build-a-app')
-    },
-    devServer: {
-        port: 4200,
-        historyApiFallback: {
-            index: '/index.html',
-            disableDotRule: true,
-            htmlAcceptHeaders: ['text/html', 'application/xhtml+xml']
-        }
-    },
-    plugins: [
-        new NxAppWebpackPlugin({
-            tsConfig: './tsconfig.app.json',
-            compiler: 'babel',
-            main: './src/main.tsx',
-            index: './src/index.html',
-            baseHref: '/',
-            assets: ['./src/favicon.ico', './src/assets'],
-            styles: [],
-            outputHashing:
-                process.env['NODE_ENV'] === 'production' ? 'all' : 'none',
-            optimization: process.env['NODE_ENV'] === 'production'
-        }),
-        new NxReactWebpackPlugin({
-            // Uncomment this line if you don't want to use SVGR
-            // See: https://react-svgr.com/
-            // svgr: false
-        })
-    ]
+const pluginOption = {
+    index: 'apps/build-a-app/src/index.html',
+    main: 'apps/build-a-app/src/main.tsx',
+    tsConfig: 'apps/build-a-app/tsconfig.app.json',
+    outputPath: 'dist/apps/build-a-app',
+    outputHashing: process.env['NODE_ENV'] === 'production' ? 'all' : 'none',
+    optimization: process.env['NODE_ENV'] === 'production',
+    assets: ['apps/build-a-app/src/favicon.ico', 'apps/build-a-app/src/assets'],
+    compiler: 'babel',
+    styles: []
 };
+
+// Nx composable plugins for webpack.
+module.exports = composePlugins(
+    withNx(pluginOption),
+    withWeb(),
+    withReact(),
+    (config, { options, context, configuration }) => {
+        config.devServer = {
+            port: 4200,
+            historyApiFallback: {
+                index: '/index.html',
+                disableDotRule: true,
+                htmlAcceptHeaders: ['text/html', 'application/xhtml+xml']
+            }
+        };
+
+        runWebpackDevServer(config);
+
+        return config;
+    }
+);
